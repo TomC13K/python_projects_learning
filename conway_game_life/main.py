bb8c21e61112e87c178e3a4087542f94be98467c
ForkTest <testFork@email.com>
2023-10-02 11:45:36 +0100

    added a cell drawing functionality

--- conway_game_life/main.py
+++ conway_game_life/main.py
@@ -20,6 +20,12 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
 def draw_grid(positions):
+    # positions-  in the grid not pixels so need to translate those to pixels to draw
+    for position in positions:
+        col,row = position         # decomposing to positions
+        top_left = (col * TILE_SIZE, row * TILE_SIZE)
+        pygame.draw.rect(screen, YELLOW, (top_left, (TILE_SIZE, TILE_SIZE)))
+
     # draw the grid lines
     for row in range(GRID_HEIGHT):
         # y is increasing going down the rows
@@ -30,17 +36,19 @@ def draw_grid(positions):
         pygame.draw.line(screen, "BLACK", (col * TILE_SIZE, 0 ), (col * TILE_SIZE, HEIGHT))
 
 
+
 def main():
     running = True
 
     positions = set()
+    positions.add((10,10))
     while running:
-        clock.tick(FPS)                     # when active, the loop run at max 60 fps
+        clock.tick(FPS)                           # when active, the loop run at max 60 fps
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:    # IF USER EXIT THE WINDOW
+            if event.type == pygame.QUIT:         # IF USER EXIT THE WINDOW
                 running = False
-        screen.fill(GREY)                          # bg color
+        screen.fill(GREY)                         # bg color
         draw_grid(positions)                      # rendering in order so bg before gridline, or bg will voer it
         pygame.display.update()                   # update the screen after the changes.or redrawing
 
